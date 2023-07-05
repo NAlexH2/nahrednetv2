682ba9ba8fe072eca2ddd2a73697dd8f602e9184
NAlexH2 <alex@nah-red.net>
2023-07-05 15:53:18 -0700

    more staging

---
+++ src/websrc/body.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function NahRedNet() {
+  return (
+    <div className="NahRedNet">
+        <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default NahRedNet;
