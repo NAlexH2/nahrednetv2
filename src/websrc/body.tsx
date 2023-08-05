a7be191583603c6dc4cbf545c6389c191dc10ce7
NAlexH2 <alex@nah-red.net>
2023-08-04 19:59:57 -0700

    prog

--- src/websrc/body.tsx
+++ src/websrc/body.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
+import '@fontsource/square-peg/';
+import '@fontsource-variable/josefin-sans';
 
 function NahRedNet() {
   return (
     <div className="NahRedNet">
-        <h1>Hello</h1>
+        <h1>Alex Harris</h1>
     </div>
   );
 }
