396c8a2837a5667497bd6c5aab7ebd5fe8184bba
NAlexH2 <alex@nah-red.net>
2023-08-08 15:23:44 -0700

    Lots of learning progress

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import dynamic from "next/dynamic";
+const Layout = dynamic(() => import("../components/layout"), { ssr: false });
+
+export default function Contact() {
+  return (
+    <Layout>
+      <h1>Contact page</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
