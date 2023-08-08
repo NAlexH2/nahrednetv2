a8cddffaa4bdc474a0a59c9ed8345bb4742fd721
NAlexH2 <alex@nah-red.net>
2023-08-08 10:00:13 -0700

    moved to nextjs. still wip

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// import Layout, { siteTitle } from "../components/layout";
+// import utilStyles from "../styles/utils.module.css";
+import Header from "./header.js"
+
+export default function Home() {
+    return(
+        <Header/>
+    )
+}
