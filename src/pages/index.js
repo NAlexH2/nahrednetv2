3eb6e909e3abb3564f372c5fd1fd7b91b856022b
NAlexH2 <alex@nah-red.net>
2023-08-08 17:16:25 -0700

    small progress towards just navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 import dynamic from "next/dynamic";
 import styles from "../styles/styles.module.css";
 import Link from "next/link";
-
+import { useRouter } from "next/router";
 const Layout = dynamic(() => import("../components/layout"), { ssr: false });
-
 const contactLink = "/contact";
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className={styles.mynav}>test</h1>
-      <Link href={contactLink} prefetch>
-        Contact
-      </Link>
+      <span className="flex justify-center">
+        <Link href={contactLink} prefetch>
+          Contact
+        </Link>
+      </span>
+      <h1 className={styles.mynav}>bio</h1>
     </Layout>
   );
 }
