fec787ffa2f17f2861d14f86d3926f12623dc823
NAlexH2 <alex@nah-red.net>
2023-08-15 01:26:18 -0700

    more imports to support fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Nahrednet from "./Nahrednet";
-import "./styles/index";
-
+import "./styles/global.css"
+import "./styles/";
+import "./fonts/JosefinSans-Italic-VariableFont_wght.ttf";
+import "./fonts/JosefinSans-VariableFont_wght.ttf";
+import "./fonts/SquarePeg-Reg.ttf";
 
 
 const root = ReactDOM.createRoot(
