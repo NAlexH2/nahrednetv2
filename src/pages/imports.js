5fa19ac9eaf24c4aadf151602a046a726ec05a61
NAlexH2 <alex@nah-red.net>
2023-08-14 20:53:21 -0700

    simplify imports a little bit...

---
+++ src/pages/imports.js
@@ -0,0 +1,6 @@
+import Index from "@/pages/index";
+import Skills from "@/pages/skills";
+import Projects from "@/pages/projects";
+import Contact from "@/pages/contact";
+
+export { Index, Skills, Projects, Contact };
