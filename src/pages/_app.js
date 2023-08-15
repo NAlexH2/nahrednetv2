99cabe8846b59b92bedec04b8e28008f53e28be4
NAlexH2 <alex@nah-red.net>
2023-08-14 20:27:29 -0700

    root layout changes are preem

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
+import dynamic from "next/dynamic";
+
+const Layout = dynamic(() => import("../components/layout"), { ssr: false });
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
