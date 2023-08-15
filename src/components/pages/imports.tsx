1d01f79d3de4a1b66f8dc48453e1de7c961b77a3
NAlexH2 <alex@nah-red.net>
2023-08-15 09:56:48 -0700

    Adding pages as components

---
+++ src/components/pages/imports.tsx
@@ -0,0 +1,6 @@
+import Index from "./index";
+import Skills from "./skills";
+import Projects from "./projects";
+import Contact from "./contact";
+
+export { Index, Skills, Projects, Contact };
---
+++ src/components/pages/skills.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { styles } from "styles";
+
+export const Skills = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.mynav}>Skills page</h1>
+    </div>
+  );
+};
+
+export default Skills;
