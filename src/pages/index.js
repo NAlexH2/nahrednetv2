df46c6dfd18f4c272a44239b1640202107ab42c1
NAlexH2 <alex@nah-red.net>
2023-08-14 20:53:30 -0700

    Minor updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
-import dynamic from "next/dynamic";
+import React from "react";
 import styles from "../styles/styles.module.css";
 import Link from "next/link";
-import { useRouter } from "next/router";
-const Layout = dynamic(() => import("../components/layout"), { ssr: false });
-const contactLink = "/contact";
 
-export default function Home() {
-  const router = useRouter();
-
+export const Index = () => {
   return (
-    <Layout>
-      <span className="flex justify-center">
-        <Link href={contactLink}>Contact</Link>
-      </span>
+    <>
       <h1 className={styles.mynav}>bio</h1>
-    </Layout>
+    </>
   );
-}
+};
+
+export default Index;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,13 @@
+import React from "react";
+import styles from "../styles/styles.module.css";
 import Link from "next/link";
-import dynamic from "next/dynamic";
-const Layout = dynamic(() => import("../components/layout"), { ssr: false });
 
-export default function Contact() {
+export const Projects = () => {
   return (
-    <Layout>
-      <h1>Projects</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </Layout>
+    <>
+      <h1 className={styles.mynav}>Projects page</h1>
+    </>
   );
-}
+};
+
+export default Projects;
