7866aca0f8dfffb65b4ee27a41f7b29ec7c85237
NAlexH2 <alex@nah-red.net>
2023-08-15 15:34:03 -0700

    Minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Nahrednet from "./Nahrednet";
-import "./styles/global.css"
-import "./styles/";
+import "./styles/global.css";
+import "./styles";
 import "./fonts/JosefinSans-Italic-VariableFont_wght.ttf";
 import "./fonts/JosefinSans-VariableFont_wght.ttf";
 import "./fonts/SquarePeg-Reg.ttf";
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Nahrednet />
   </React.StrictMode>
 );
-
