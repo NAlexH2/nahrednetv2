6e75b300a662d4c264a9ef2240bcd2c9a34b0faf
NAlexH2 <alex@nah-red.net>
2023-08-15 20:10:35 -0700

    No longer required for this branch

--- nahrednet-nextjs-old/src/pages/imports.js
+++
@@ -1,6 +0,0 @@
-import Index from "@/pages/index";
-import Skills from "@/pages/skills";
-import Projects from "@/pages/projects";
-import Contact from "@/pages/contact";
-
-export { Index, Skills, Projects, Contact };
