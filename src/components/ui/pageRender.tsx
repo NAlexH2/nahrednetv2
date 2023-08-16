399c9e3d8dced9f33c3a540c138fc797436cf688
NAlexH2 <alex@nah-red.net>
2023-08-15 22:32:22 -0700

    typing added

--- src/components/ui/pageRender.tsx
+++ src/components/ui/pageRender.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Index, Skills, Projects, Contact } from "components/pages/imports";
 
-const index = "index";
-const skills = "skills";
-const projects = "projects";
-const contact = "contact";
+const index: string = "index";
+const skills: string = "skills";
+const projects: string = "projects";
+const contact: string = "contact";
 
 const PageRender = (pageIdent: String) => {
-
-    if (pageIdent === index) {
-        return <Index />;
-    } else if (pageIdent === skills) {
-        return <Skills />;
-    } else if (pageIdent === projects) {
-        return <Projects />;
-    } else if (pageIdent === contact) {
-        return <Contact />;
-    }
+  if (pageIdent === index) {
+    return <Index />;
+  } else if (pageIdent === skills) {
+    return <Skills />;
+  } else if (pageIdent === projects) {
+    return <Projects />;
+  } else if (pageIdent === contact) {
+    return <Contact />;
+  }
 };
 
 export default PageRender;
