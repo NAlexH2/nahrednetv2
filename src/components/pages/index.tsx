aa3233e6a79596344a956135e02f583e3260eaa0
NAlexH2 <alex@nah-red.net>
2023-08-15 22:26:39 -0700

    draft picture in bio

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { styles } from "styles";
 
-
+const headshot: string = "images/hs2-small.jpg";
 export const Index = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.mynav}>Bio</h1>
+    <div className={`${styles.container}`}>
+      <div>
+        <img
+          id="myface"
+          className={styles.headshot}
+          src={headshot}
+          alt="Alex Harris"
+        />
+      </div>
+      <div>
+        <h1 className={styles.mynav}>Bio</h1>
+      </div>
     </div>
   );
 };
