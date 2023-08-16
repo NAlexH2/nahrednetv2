b65763ce6aab4f929fcd6a609cf6d8017476166a
NAlexH2 <alex@nah-red.net>
2023-08-15 20:02:07 -0700

    text transform is dumb

--- src/components/ui/mButton.tsx
+++ src/components/ui/mButton.tsx
@@ -3,23 +3,24 @@ import { Button } from "@mui/material";
 
 interface MButtonProps {
   text?: string;
-  className?: string;
-  id?: string;
+  className?: string | undefined;
+  id?: string | undefined;
   onClick?: () => void;
-  children?: React.ReactNode;
+  children?: React.ReactNode | undefined;
 }
 
 const MButton = (prop: MButtonProps) => {
   return (
     <Button
       variant="contained"
-      className={prop.className}
-      id={prop.id}
+      className={prop.className !== undefined ? prop.className : undefined}
+      id={prop.id !== undefined ? prop.id : undefined}
       onClick={prop.onClick}
       sx={{
         "&:hover": { backgroundColor: "transparent", boxShadow: "none" },
         backgroundColor: "transparent",
         boxShadow: "none",
+        textTransform: "none",
       }}
       disableRipple
     >
