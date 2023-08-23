925357c28718f6f5e80933b060dbf0d28695976c
NAlexH2 <alex@nah-red.net>
2023-08-23 13:25:04 -0700

    ez imports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,18 @@
+import NavBar from "./navBar";
+import NRNButton from "./nrnButton";
+import NRNImage from "./nrnImage";
+import SystemTimeGreeting from "./systemTimeGreeting";
+import PageHeader from "./pageHeader";
+import { SquarePegTrue, Josefin } from "../../fonts/fonts";
+import CurrentNavPage from "../utils/currentNavPage";
+
+export {
+  NavBar,
+  NRNButton,
+  NRNImage,
+  SystemTimeGreeting,
+  PageHeader,
+  SquarePegTrue,
+  Josefin,
+  CurrentNavPage,
+};
