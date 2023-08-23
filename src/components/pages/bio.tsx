f8668ae24b939735d6b34ba33fa2a11562e44508
NAlexH2 <alex@nah-red.net>
2023-08-23 16:17:13 -0700

    more tailwind wip

--- src/components/pages/bio.tsx
+++ src/components/pages/bio.tsx
@@ -7,26 +7,28 @@ import Headshot from "/public/images/hs2-small.jpg";
 const Bio = () => {
   return (
     <div className="flex justify-center m-12">
-      <span className="columns-5 ml-16"></span>
+      <span className="lg:columns-5 ml-16"></span>
 
-      <div className=" h-auto w-auto">
+      <div className="object-scale-down m-4">
         <NRNImage
-          className={`${styles.headshot} object-scale-down`}
+          className={`${styles.headshot}`}
           src={Headshot}
           alt="Alex Harris"
         />
       </div>
 
-      <div className={`${styles.about}`}>
-        <Greeting />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi,
-        sequi, neque fuga consectetur alias inventore deserunt ullam distinctio
-        rerum maxime. Illo non odit sint temporibus porro laudantium iusto quae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat at id
-        facilis fugit ut labore. Ex accusamus cupiditate dignissimos quasi quam
-        qui adipisci eum? Voluptatum tempora sint voluptatem est hic?
+      <div className="mr-16 max-w-3xl">
+        <div className={`m-5 ${styles.about}`}>
+          <Greeting />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi,
+          sequi, neque fuga consectetur alias inventore deserunt ullam
+          distinctio rerum maxime. Illo non odit sint temporibus porro
+          laudantium iusto quae! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Repellat at id facilis fugit ut labore. Ex accusamus
+          cupiditate dignissimos quasi quam qui adipisci eum? Voluptatum tempora
+          sint voluptatem est hic?
+        </div>
       </div>
-      <span className="mr-16"></span>
     </div>
   );
 };
