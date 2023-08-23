a54a45272c240b024ad1f556ec3f38cea6fb50a6
NAlexH2 <alex@nah-red.net>
2023-08-23 15:48:16 -0700

    progress with tailwind!

--- src/components/pages/bio.tsx
+++ src/components/pages/bio.tsx
@@ -6,20 +6,27 @@ import Headshot from "/public/images/hs2-small.jpg";
 
 const Bio = () => {
   return (
-    <div className={`${styles.container}`}>
-      <div className="flex">
+    <div className="flex justify-center m-12">
+      <span className="columns-5 ml-16"></span>
+
+      <div className=" h-auto w-auto">
         <NRNImage
-          id={styles.myface}
-          className={`${styles.headshot}`}
+          className={`${styles.headshot} object-scale-down`}
           src={Headshot}
           alt="Alex Harris"
         />
-
-        <div className={`${styles.bio} mx-16`}>
-          <Greeting />
-          Bio
-        </div>
       </div>
+
+      <div className={`${styles.about}`}>
+        <Greeting />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi,
+        sequi, neque fuga consectetur alias inventore deserunt ullam distinctio
+        rerum maxime. Illo non odit sint temporibus porro laudantium iusto quae!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat at id
+        facilis fugit ut labore. Ex accusamus cupiditate dignissimos quasi quam
+        qui adipisci eum? Voluptatum tempora sint voluptatem est hic?
+      </div>
+      <span className="mr-16"></span>
     </div>
   );
 };
