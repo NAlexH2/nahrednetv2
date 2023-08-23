b9e340003c5b9f6219b0687e7d9ce87c7a449f1d
NAlexH2 <alex@nah-red.net>
2023-08-23 13:24:59 -0700

    testing some things

--- src/components/pages/bio.tsx
+++ src/components/pages/bio.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { styles } from "styles";
 import NRNImage from "components/ui/nrnImage";
+import Greeting from "components/ui/systemTimeGreeting";
 import Headshot from "/public/images/hs2-small.jpg";
 
-export const Bio = () => {
+const Bio = () => {
   return (
     <div className={`${styles.container}`}>
-      <div className="flex justify-center">
+      <div className="flex">
         <NRNImage
           id={styles.myface}
           className={`${styles.headshot}`}
           src={Headshot}
           alt="Alex Harris"
         />
-        <h1 className={`${styles.bio} mx-16`}>Bio</h1>
+
+        <div className={`${styles.bio} mx-16`}>
+          <Greeting />
+          Bio
+        </div>
       </div>
     </div>
   );
