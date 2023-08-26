94e7e6cc15247d08872fc11679d577e4f92c8f1c
NAlexH2 <alex@nah-red.net>
2023-08-25 23:32:18 -0700

    ez imports

---
+++ src/components/ui/bioElements/index.tsx
@@ -0,0 +1,6 @@
+import BioBegin from "./bioBegin";
+import FriendsDescribe from "./friendsDescribe";
+import PastBio from "./pastBio";
+import PresentBio from "./presentBio";
+
+export { BioBegin, FriendsDescribe, PastBio, PresentBio };
