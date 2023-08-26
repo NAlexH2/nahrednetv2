cce91b95feeffe69fc5d54ad513147964c3967ee
NAlexH2 <alex@nah-red.net>
2023-08-25 23:32:24 -0700

    wip

---
+++ src/components/ui/bioElements/pastBio.tsx
@@ -0,0 +1,13 @@
+import { styles } from "styles";
+import Link from "next/link";
+
+export const PastBio = () => {
+  return (
+    <div>
+      <br />
+      <text>test</text>
+    </div>
+  );
+};
+
+export default PastBio;
