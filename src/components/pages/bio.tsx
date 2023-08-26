02544d8c3fc61f29fa8b397ff642354384ac6322
Alex Harris <nharris@pdx.edu>
2023-08-25 17:55:52 -0700

    Progress on recreating desired design

--- src/components/pages/bio.tsx
+++ src/components/pages/bio.tsx
@@ -6,28 +6,30 @@ import Headshot from "/public/images/hs2-small.jpg";
 
 const Bio = () => {
   return (
-    <div className="flex justify-center m-12">
-      <span className="lg:columns-5 ml-16"></span>
+    <div className="flex justify-center">
+      <div className="md:flex justify-center md:ml-7 md:mr-12 md:mb-8">
+        {/* <span className="sm:columns-5 xl:mr-16 h-auto"></span> */}
 
-      <div className="object-scale-down m-4">
-        <NRNImage
-          className={`${styles.headshot}`}
-          src={Headshot}
-          alt="Alex Harris"
-        />
-      </div>
-
-      <div className="mr-16 max-w-3xl">
-        <div className={`m-5 ${styles.about}`}>
-          <Greeting />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi,
-          sequi, neque fuga consectetur alias inventore deserunt ullam
-          distinctio rerum maxime. Illo non odit sint temporibus porro
-          laudantium iusto quae! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Repellat at id facilis fugit ut labore. Ex accusamus
-          cupiditate dignissimos quasi quam qui adipisci eum? Voluptatum tempora
-          sint voluptatem est hic?
+        <div className="justify-center max-md:flex md:mr-5 max-md:mx-10">
+          <NRNImage
+            className={`${styles.headshot} max-h-72 w-auto`}
+            src={Headshot}
+            alt="Alex Harris"
+          />
         </div>
+        <div className="">
+          <div className={`${styles.about}`}>
+            <Greeting />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            eligendi, sequi, neque fuga consectetur alias inventore deserunt
+            ullam distinctio rerum maxime. Illo non odit sint temporibus porro
+            laudantium iusto quae! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Repellat at id facilis fugit ut labore. Ex
+            accusamus cupiditate dignissimos quasi quam qui adipisci eum?
+            Voluptatum tempora sint voluptatem est hic?
+          </div>
+        </div>
+        {/* <span className="xl:columns-5 xl:ml-16"></span> */}
       </div>
     </div>
   );
