5f8beb39025ea24bad6ab9ad54a663a154661b9e
NAlexH2 <alex@nah-red.net>
2023-08-25 23:32:08 -0700

    moving into own component

---
+++ src/components/ui/bioElements/friendsDescribe.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PastBio } from ".";
+
+export const FriendsDescribe = () => {
+  return (
+    <>
+      <text>
+        When I&#39;ve spoken to my friends about how we see one another, a few
+        words that are frequent descriptors have been both &#34;resilient&#34;
+        and &#34;soft-spoken&#34;. Attributes that I never really considered for
+        myself, but am certainly not opposed to. These haven&#39;t always been
+        the case, but my experience has certainly helped shape who I am today,
+        and made me a much better person.
+      </text>
+    </>
+  );
+};
+
+export default FriendsDescribe;
