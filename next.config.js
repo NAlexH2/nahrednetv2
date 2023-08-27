4e266543b48e143a0b503ce7f8fe8e913acdef41
NAlexH2 <alex@nah-red.net>
2023-08-26 21:03:53 -0700

    testing something

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
    */
   images: {
-    unoptimized: true,
+    // unoptimized: true,
   },
 };
 
