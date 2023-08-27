6612863ef5373acab9193708df2499b29fe76786
NAlexH2 <alex@nah-red.net>
2023-08-26 20:15:29 -0700

    creating button for page footer

---
+++ src/components/ui/pageFooter/gitHubButton.tsx
@@ -0,0 +1,21 @@
+import { NRNButton } from "../nrnButtons";
+import { NRNImage } from "../imageComponents";
+import GitHub from "/public/images/github.svg";
+import Link from "next/link";
+export const GitHubButton = () => {
+  return (
+    <div>
+      <Link href="https://github.com/NAlexH2" legacyBehavior>
+        <a target="_blank" rel="noopener noreferrer">
+          <NRNImage
+            className="w-14"
+            src={GitHub}
+            alt="A Button with the LinkedIn logo"
+          />
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default GitHubButton;
