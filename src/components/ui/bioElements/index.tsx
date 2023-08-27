c126336073c282d2b35c2e9d2db28d54c4ac0ed3
NAlexH2 <alex@nah-red.net>
2023-08-26 20:12:33 -0700

    file renamed

--- src/components/ui/bioElements/index.tsx
+++
@@ -1,6 +0,0 @@
-import BioBegin from "./bioBegin";
-import FriendsDescribe from "./friendsDescribe";
-import PastBio from "./pastBio";
-import PresentBio from "./presentBio";
-
-export { BioBegin, FriendsDescribe, PastBio, PresentBio };
