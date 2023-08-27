ec2ead2ba089d900f3f3b921cba4d39e7590b452
NAlexH2 <alex@nah-red.net>
2023-08-26 18:36:07 -0700

    text added

--- src/components/ui/bioElements/pastBio.tsx
+++ src/components/ui/bioElements/pastBio.tsx
@@ -3,10 +3,42 @@ import Link from "next/link";
 
 export const PastBio = () => {
   return (
-    <div>
+    <p className="text-justify">
+      I found that College after High School was not the path for me - not yet
+      at least. I spent 8 years active duty in the US Navy as an Electronics
+      Technician where I worked on both internal and external means of
+      communication: radios, SONAR, cryptographic equipment and a patch system
+      that managed all external communications on these systems. By the time my
+      first 4 years had finished I made the enlisted rank of Petty Officer
+      Second Class in a highly competitive job for the Navy, where at the time
+      across the entire Navy the advancement rate was around 3% for anyone
+      eligible.
       <br />
-      <text>test</text>
-    </div>
+      <br />
+      By the end of my second 4 years, I had worked on and executed
+      multi-million-dollar projects, became the ships expert on technical
+      documentation, and was a highly trusted junior enlisted Sailor overseas in
+      Japan. While stationed in Japan, I was a part of a team responsible for
+      docking and undocking ships, submarines, and barges as well as managing
+      helicopter flight operations.
+      <br />
+      <br />
+      Upon finishing my time in the Navy, I returned to Portland State as the
+      COVID pandemic started in the Spring of 2020. Since then I've taken
+      classes that have taught me foundational programming knowledge but also
+      algorithms and testing methodologies. For more on these please visit my
+      skills page.
+      <br />
+      <br />
+      During the Summer of 2022, I took 16 credits in order to finalize my
+      graduation date and set my future up to be in a better position than what
+      it would have been. Doing this was hard work and required a lot of
+      dedication to accomplish outstanding grades in a matter of eight weeks for
+      four classes - Calculus 3, Statistics, Introduction to Web Development,
+      and Advanced Programming with Java. I finished off the term with an
+      Advanced Programming with Java. I finished off the term with an 'A' letter
+      grade for each class.
+    </p>
   );
 };
 
--- src/components/ui/bioElements/presentBio.tsx
+++ src/components/ui/bioElements/presentBio.tsx
@@ -3,7 +3,16 @@ import { arch } from "os";
 export const PresentBio = () => {
   return (
     <>
-      <h2></h2>
+      <p>
+        As of the Summer of 2023 - and due to a variety of circumstances - I am
+        simply student worker as a Graduate student at Portland State University
+        where I am furthering my knowledge and begin working towards my Masters
+        in Computer Science starting Fall term, 2023 and finishing by Summer
+        2024. My plan is to take a large variety of courses that explore both
+        Software Engineering and Artificial Intelligence. I currently am focused
+        on self-teaching and exploring React through various supporting
+        frameworks such as NextJS and React Native over my summer break.{" "}
+      </p>
     </>
   );
 };
