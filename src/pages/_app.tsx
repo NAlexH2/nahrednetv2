0402c82f528384ce0d198ab50b327b71d24eba36
NAlexH2 <alex@nah-red.net>
2023-08-28 16:28:41 -0700

    layout done in index.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,5 @@
 import { AppProps } from "next/app";
 import "styles/global.css";
-import dynamic from "next/dynamic";
-
-const Layout = dynamic(() => import("../components/layout"), { ssr: false });
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
