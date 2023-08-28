37384cfa99d6f03ba56661447321e9f8d559f061
NAlexH2 <alex@nah-red.net>
2023-08-28 14:02:50 -0700

    staging

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -3,8 +3,12 @@ import { styles } from "styles";
 
 export const Projects = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={`${styles.mynav}`}>Projects page</h1>
+    <div className="container mx-auto">
+      <div>
+        <text className="md:flex justify-center md:mx-12 mb-7 max-md:px-10">
+          <h1 className={styles.mynav}>Projects page</h1>
+        </text>
+      </div>
     </div>
   );
 };
