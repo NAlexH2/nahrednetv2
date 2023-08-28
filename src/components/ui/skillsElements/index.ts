dba8ad43d97767a50b8a2011a29ab8567f37771a
NAlexH2 <alex@nah-red.net>
2023-08-28 16:28:29 -0700

    ez imports

---
+++ src/components/ui/skillsElements/index.ts
@@ -0,0 +1,6 @@
+import ClassesComplete from "./classesComplete";
+import PastWork from "./pastWork";
+import SoftSkills from "./softSkills";
+import TechnicalSkills from "./technicalSkills";
+
+export { ClassesComplete, PastWork, SoftSkills, TechnicalSkills };
