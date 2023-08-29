2a0a08b5483937a788c5f774df795121187413b8
NAlexH2 <alex@nah-red.net>
2023-08-28 22:13:24 -0700

    no longer required

--- src/components/ui/skillsElements/index.ts
+++
@@ -1,6 +0,0 @@
-import ClassesComplete from "./classesComplete";
-import PastWork from "./pastWork";
-import SoftSkills from "./softSkills";
-import TechnicalSkills from "./technicalSkills";
-
-export { ClassesComplete, PastWork, SoftSkills, TechnicalSkills };
