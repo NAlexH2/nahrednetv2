1b59807b491551be3594a58fdf50061b1f422fb8
NAlexH2 <alex@nah-red.net>
2023-08-28 22:13:01 -0700

    text is not a valid html element. divs it is.

--- src/components/pages/skills.tsx
+++ src/components/pages/skills.tsx
@@ -1,16 +1,41 @@
-import { TechnicalSkills } from "components/ui/skillsElements";
 import React from "react";
+import SkillsBox from "components/ui/skillsElements/skillsBox";
 import { styles } from "styles";
+import {
+  knownLangs,
+  knownTools,
+  softSkills,
+  hardSkills,
+  completedCourses,
+} from "components/ui/skillsElements/skillsLists";
 
 export const Skills = () => {
   return (
     <div className="container mx-auto">
       <div>
-        <text className="md:flex justify-center md:mx-12 mb-7 max-md:px-10">
+        <div className="md:flex justify-center md:mx-12 mb-7 max-md:px-10">
           <h1 className={styles.mynav}>Skills page</h1>
-        </text>
+        </div>
       </div>
-      <TechnicalSkills></TechnicalSkills>
+      <li className="flex justify-center">
+        <div className="pr-2">
+          <SkillsBox listedItems={knownLangs} />
+        </div>
+        <div className="pr-2">
+          <SkillsBox listedItems={knownTools} />
+        </div>
+        <div>
+          <SkillsBox listedItems={softSkills} />
+        </div>
+      </li>
+      <li className="flex justify-center mt-4">
+        <div className="pr-2">
+          <SkillsBox listedItems={hardSkills} />
+        </div>
+        <div>
+          <SkillsBox listedItems={completedCourses} />
+        </div>
+      </li>
     </div>
   );
 };
--- src/components/ui/bioElements/friendsDescribe.tsx
+++ src/components/ui/bioElements/friendsDescribe.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 export const FriendsDescribe = () => {
   return (
-    <>
-      <text>
-        When I&#39;ve spoken to my friends about how we see one another, a few
-        words that are frequent descriptors have been both &#34;resilient&#34;
-        and &#34;soft-spoken&#34;. Attributes that I never really considered for
-        myself, but am certainly not opposed to. These haven&#39;t always been
-        the case, but my experience has certainly helped shape who I am today,
-        and made me a much better person.
-      </text>
-    </>
+    <div>
+      When I&#39;ve spoken to my friends about how we see one another, a few
+      words that are frequent descriptors have been both &#34;resilient&#34; and
+      &#34;soft-spoken&#34;. Attributes that I never really considered for
+      myself, but am certainly not opposed to. These haven&#39;t always been the
+      case, but my experience has certainly helped shape who I am today, and
+      made me a much better person.
+    </div>
   );
 };
 
