cfc1a12a5e0bc9ad8d53792444ad486429b43f06
NAlexH2 <alex@nah-red.net>
2023-08-29 19:05:35 -0700

    Found fix to use Bio instead of Index

--- src/components/ui/pageHeader/pageHeader.tsx
+++ src/components/ui/pageHeader/pageHeader.tsx
@@ -3,8 +3,6 @@ import { NRNImage } from "../imageComponents";
 import { styles } from "styles";
 import beard from "/public/images/beard.svg";
 
-const index: string = "index";
-
 interface NavBarProps {
   onUpdate: (data: string) => void;
 }
@@ -12,7 +10,7 @@ interface NavBarProps {
 const PageHeader = ({ onUpdate }: NavBarProps) => {
   return (
     <header className={"flex justify-center mt-2 max-md:mb-4 max-sm:-ml-6"}>
-      <NRNButton onClick={() => onUpdate(index)}>
+      <NRNButton onClick={() => onUpdate("Bio")}>
         <NRNImage id={styles.mynavlogo} src={beard} alt="website logo" />
         <div id={styles.imagenavtext}>Alex Harris</div>
       </NRNButton>
