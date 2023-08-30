47000d9b3bd03dc663de8ce2f0e3a8ae9d62fc87
NAlexH2 <alex@nah-red.net>
2023-08-29 19:04:14 -0700

    super simple now

--- src/components/ui/navBar/navBar.tsx
+++ src/components/ui/navBar/navBar.tsx
@@ -1,50 +1,19 @@
-import { styles } from "styles";
 import { NRNButton } from "../nrnButtons";
-
-const index: string = "index";
-const skills: string = "skills";
-const projects: string = "projects";
-const contact: string = "contact";
 
 interface NavBarProps {
   onUpdate: (data: string) => void;
   thisPage: string;
+  id?: string;
 }
 
-export const NavBar = ({ onUpdate, thisPage }: NavBarProps) => {
-  const updateCurrentNavStyle = (toChange: string) => {
-    return thisPage === toChange ? styles.currentnav : styles.navtext;
-  };
+export const NavBar = ({ onUpdate, thisPage, id }: NavBarProps) => {
   return (
-    <nav className={`${styles.mynav} flex justify-center mb-7`}>
-      <NRNButton
-        className="max-md:mr-0.5 md:mr-8"
-        id={updateCurrentNavStyle(index)}
-        onClick={() => onUpdate(index)}
-      >
-        Bio
-      </NRNButton>
-      <NRNButton
-        className="max-md:mr-3 md:mr-8"
-        id={updateCurrentNavStyle(skills)}
-        onClick={() => onUpdate(skills)}
-      >
-        Skills
-      </NRNButton>
-      <NRNButton
-        className="max-md:mr-3 md:mr-8"
-        id={updateCurrentNavStyle(projects)}
-        onClick={() => onUpdate(projects)}
-      >
-        Projects
-      </NRNButton>
-      <NRNButton
-        id={updateCurrentNavStyle(contact)}
-        onClick={() => onUpdate(contact)}
-      >
-        Contact
-      </NRNButton>
-    </nav>
+    <NRNButton
+      className="max-md:mr-0.5 md:mr-8"
+      id={id}
+      onClick={() => onUpdate(thisPage)}
+      text={thisPage}
+    />
   );
 };
 
