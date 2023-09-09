6d99ac0c408cbe739f58a4e404c959432ce30304
Alex Harris <nharris@pdx.edu>
2023-09-08 20:05:09 -0700

    Adding optional class to navbar

--- src/components/ui/navBar/navBar.tsx
+++ src/components/ui/navBar/navBar.tsx
@@ -4,12 +4,13 @@ interface NavBarProps {
   onUpdate: (data: string) => void;
   thisPage: string;
   id?: string;
+  className?: string | undefined;
 }
-
-export const NavBar = ({ onUpdate, thisPage, id }: NavBarProps) => {
+const defaultNavBarButtonStyle = "max-md:mr-0.5 md:mr-8";
+export const NavBar = ({ onUpdate, thisPage, id, className }: NavBarProps) => {
   return (
     <NRNButton
-      className="max-md:mr-0.5 md:mr-8"
+      className={className === undefined ? defaultNavBarButtonStyle : className}
       id={id}
       onClick={() => onUpdate(thisPage)}
       text={thisPage}
