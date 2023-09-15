db0c4031e1f30f092f9bbb9b3be6f1b9ab632bc1
NAlexH2 <alex@nah-red.net>
2023-09-15 11:08:48 -0700

    unused file

--- src/components/ui/skillsElements/classesComplete.tsx
+++
@@ -1,5 +0,0 @@
-export const ClassesComplete = () => {
-  return <></>;
-};
-
-export default ClassesComplete;
--- src/components/ui/skillsElements/pastWork.tsx
+++
@@ -1,5 +0,0 @@
-export const PastWork = () => {
-  return <></>;
-};
-
-export default PastWork;
