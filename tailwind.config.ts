459302f7d2424dd8fc35f06be965f65e88efb5b5
NAlexH2 <alex@nah-red.net>
2023-09-18 13:31:32 -0700

    adding more a lot more columns

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,55 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))",
+        "14": "repeat(14, minmax(0, 1fr))",
+        "15": "repeat(15, minmax(0, 1fr))",
+        "16": "repeat(16, minmax(0, 1fr))",
+        "17": "repeat(17, minmax(0, 1fr))",
+        "18": "repeat(18, minmax(0, 1fr))",
+        "19": "repeat(19, minmax(0, 1fr))",
+        "20": "repeat(20, minmax(0, 1fr))",
+        "21": "repeat(21, minmax(0, 1fr))",
+        "22": "repeat(22, minmax(0, 1fr))",
+        "23": "repeat(23, minmax(0, 1fr))",
+        "24": "repeat(24, minmax(0, 1fr))",
+        "25": "repeat(25, minmax(0, 1fr))",
+        "26": "repeat(26, minmax(0, 1fr))",
+        "27": "repeat(27, minmax(0, 1fr))",
+        "28": "repeat(28, minmax(0, 1fr))",
+        "29": "repeat(29, minmax(0, 1fr))",
+        "30": "repeat(30, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
+        "span-17": "span 17 / span 17",
+        "span-18": "span 18 / span 18",
+        "span-19": "span 19 / span 19",
+        "span-20": "span 20 / span 20",
+        "span-21": "span 21 / span 21",
+        "span-22": "span 22 / span 22",
+        "span-23": "span 23 / span 23",
+        "span-24": "span 24 / span 24",
+        "span-25": "span 25 / span 25",
+        "span-26": "span 26 / span 26",
+        "span-27": "span 27 / span 27",
+        "span-28": "span 28 / span 28",
+        "span-29": "span 29 / span 29",
+        "span-30": "span 30 / span 30",
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
