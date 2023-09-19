b557de63bd48e7cba4d0ec298894d4ce77fee6a0
NAlexH2 <alex@nah-red.net>
2023-09-18 20:39:10 -0700

    using unsafe html

--- src/components/ui/experienceElements/accordionData.ts
+++ src/components/ui/experienceElements/accordionData.ts
@@ -11,18 +11,27 @@ const accordionData: AccordionItem[] = [
     title: "Portland State University - Technical Course Support Specialist",
     dates: "April 2021 - Present",
     content: `
-    Facilitated introductory programming labs into the field of Computer Science
-    for first year students as a peer educator helping students learn how to 
-    execute their first program to building abstract data types by practicing 
-    in the lab environment and providing meaningful constructive feedback.
+    <div class="px-6 mt-5">
+      Facilitated introductory programming labs into the field of Computer 
+      Science for first year students as a peer educator helping students learn
+      how to execute their first program to building abstract data types by
+      practicing in the lab environment and providing meaningful constructive 
+      feedback.
+    </div>
     
-    Performed training for new hires over a 10-week period each term to enable
-    smooth and successful integration in the team as well as working with
-    students through open conversations and role-playing as students with
-    broken code.
+    <div class="px-6 mt-5">
+      Performed training for new hires over a 10-week period each term to enable
+      smooth and successful integration in the team as well as working with
+      students through open conversations and role-playing as students with 
+      broken
+      code.
+    </div>
     
-    Created new course materials for all students to use and practice with in
-    the Computer Science program for their first year of lower division courses. 
+    <div class="px-6 mt-5">
+      Created new course materials for all students to use and practice with in
+      the Computer Science program for their first year of lower division 
+      courses.
+    </div>
     `,
   },
   {
@@ -30,20 +39,42 @@ const accordionData: AccordionItem[] = [
     title: "United States Navy - Electronics Technician, 2nd Class (E-5)",
     dates: "February 2012 - February 2020",
     content: `
-    USS Theodore Roosevelt:
-    February 2013 - February 2017
+    <div class="mt-7 text-2xl font-medium">USS Theodore Roosevelt:</div>
+    <div class="mt-1 ml-5 text-xl font-medium">February 2013 - February 2017</div>
+    <div class="px-6 mt-5">
+    Manager of three maintenance work centers involving tracking and performing 
+    maintenance on dozens of large systems with hundreds of maintenance action 
+    items for each system to maintain external radio communications on-board the
+    ship.
+    </div>
+
+    <div class="px-6 mt-5">
+    Created and executed meticulous inventory systems for 40,000 ships technical
+    documentation to enable accurate and time saving maintenance for countless 
+    person-hours.
+    </div>
     
-    Managed, repaired, and handled various customer requests and work center
-    duties for external communications equipment repair within several
-    communication suites.
+
+    <div class="AccordionHR"></div>
+
+    <div class="text-2xl font-medium">Commander Fleet Activities Sasebo, Japan:</div>
+    <div class="mt-1 ml-5 text-xl font-medium">May 2017 - February 2020</div>
+    <div class="px-6 mt-5">
+    Tracked, authorized, and issued approved logistical requests and 
+    requirements for naval forces for the US, Japan, Australian, Canadian, and
+    </div>
     
-    Commander Fleet Activities Sasebo, Japan:
-    May 2017 - February 2020
-    
-    Responsible for managing docking and undocking vessels from the bases births
-    ensuring safety and comfort 12 homeport ships. Scheduled and managed flight
-    operations for various kinds of aircraft for both regular duties and
-    distinguished visitors.
+    <div class="px-6 mt-5">
+    Indian naval forces during port visits to the base.
+    Conducted helicopter operations in and out of Sasebo, Japan for both VIPs,
+    and same day high priority service items to be delivered to ships at sea.
+    </div>
+  
+    <div class="px-6 mt-5">
+    Performed weekly Operations Intelligence briefings with command triad and
+    high-ranking officers stationed shipside and shore-side maintaining critical
+    strategic information within the South Pacific.
+    </div>
 
     `,
   },
