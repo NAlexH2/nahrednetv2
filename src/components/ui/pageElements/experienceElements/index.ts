184d8abb0c5cf99683d799a26abf8afa67a06dc2
NAlexH2 <alex@nah-red.net>
2023-09-22 14:21:39 -0700

    simplifying imports of these

---
+++ src/components/ui/pageElements/experienceElements/index.ts
@@ -0,0 +1,5 @@
+import accordionData from "./accordionData";
+import SkillsBox from "./skillsBox";
+import WorkAccordion from "./workAccordion";
+
+export { accordionData, SkillsBox, WorkAccordion };
