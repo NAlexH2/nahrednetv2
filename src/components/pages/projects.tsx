532c330aac4c74babdd26363998e90f8a17d247f
NAlexH2 <alex@nah-red.net>
2023-09-25 15:48:39 -0700

    removed animate

--- src/components/pages/projects.tsx
+++ src/components/pages/projects.tsx
@@ -1,17 +1,13 @@
 import { ProjectsDisplay } from "components/ui/pageComponents/projectComponents";
-import AnimateTx from "components/utils/animateTx";
 import React from "react";
-import { ClassStyles } from "styles";
 
 export const Projects = () => {
   return (
-    <AnimateTx>
-      <div className="container mx-auto max-w-[1240px] -mt-4">
-        <div>
-          <ProjectsDisplay />
-        </div>
+    <div className="container mx-auto max-w-[880px] -mt-4">
+      <div>
+        <ProjectsDisplay />
       </div>
-    </AnimateTx>
+    </div>
   );
 };
 
