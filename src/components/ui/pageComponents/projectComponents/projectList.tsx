eaaec6cef45f7c701dc22a03797f99994e329e3c
NAlexH2 <alex@nah-red.net>
2024-01-03 09:50:13 -0800

    update project list

--- src/components/ui/pageComponents/projectComponents/projectList.tsx
+++ src/components/ui/pageComponents/projectComponents/projectList.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 import { NRNV2, NRNV1, Safe, PhoneBill, HaskellGo } from "./projects";
+import CloudProject from "./projects/cloudproject";
 
 const projectLists: [string, ReactNode][] = [
-  ["nah-red.net v2", <NRNV2 key="0" />],
-  ["nah-red.net v1", <NRNV1 key="1" />],
-  ["SAFE", <Safe key="2" />],
-  ["Phone Bill", <PhoneBill key="3" />],
-  ["HaskellGo", <HaskellGo key="4" />],
+  ["Video Game Search", <CloudProject key="0" />],
+  ["nah-red.net v2", <NRNV2 key="1" />],
+  ["nah-red.net v1", <NRNV1 key="2" />],
+  ["SAFE", <Safe key="3" />],
+  ["Phone Bill", <PhoneBill key="4" />],
+  ["HaskellGo", <HaskellGo key="5" />],
 ];
 
 export default projectLists;
